c97d48cb4223e00f6a889ace6536800a24338356
Shrey Mehrotra <shreymehrotra011@gmail.com>
2026-04-26 08:58:57 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.java
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.java
@@ -9,19 +9,19 @@ class Solution {
         Arrays.sort(deck);
 
         while (indexInDeck < N) {
-            // There is an available gap in result
+            
             if (result[indexInResult] == 0) {
 
-                // Add a card to result
+                
                 if (!skip) {
                     result[indexInResult] = deck[indexInDeck];
                     indexInDeck++;
                 }
 
-                // Toggle skip to alternate between adding and skipping cards
+                
                 skip = !skip;
             }
-            // Progress to next index of result array
+            
             indexInResult = (indexInResult + 1) % N;
         }
         return result;
