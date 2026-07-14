3f45c890b705d081161d575249da560e823eda71
Shrey Mehrotra <shreymehrotra011@gmail.com>
2026-07-14 13:43:41 +0530

    Time: 15 ms (60.91%), Space: 51.3 MB (74.51%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int l = 0;
+        int r = 0;
+        while(l<s.length && r<g.length){
+            if(g[r]<=s[l]){
+                r++;
+            }
+            l++;
+        }
+        return r;
+    }
+}
