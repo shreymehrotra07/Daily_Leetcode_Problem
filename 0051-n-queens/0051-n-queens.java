743c9cbaea61957198aa00e0b2a410f3fcdca75f
Shrey Mehrotra <shreymehrotra011@gmail.com>
2026-08-09 00:42:39 +0530

    Time: 2 ms (81.58%), Space: 46.4 MB (92.48%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -17,15 +17,15 @@ class Solution {
             res.add(ans);
             return;
         }
-        for(int col=0 ; col<board.length ; col++){
-            if(isSafe(board,row,col)){
-                board[row][col]='Q';
+        for(int i=0 ; i<board.length ; i++){
+            if(isSafe(board,row,i)){
+                board[row][i] = 'Q';
                 generate(board,row+1);
-                board[row][col]='.';
+                board[row][i] = '.';
             }
         }
     }
-    public boolean isSafe(char[][] board,int row,int col){
+    public boolean isSafe(char[][] board,int row , int col){
         for(int i=0 ; i<board.length ; i++){
             if(board[i][col]=='Q'){
                 return false;
