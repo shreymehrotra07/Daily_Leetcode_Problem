5321ba68e98605fe26c54710301a39fdc6ccef9a
Shrey Mehrotra <shreymehrotra011@gmail.com>
2026-08-09 00:58:13 +0530

    Time: 2 ms (60.63%), Space: 42 MB (59.18%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -0,0 +1,55 @@
+class Solution {
+    static int res = 0;
+    public int totalNQueens(int n) {
+        res = 0;
+        char[][] board = new char[n][n];
+        for(int i=0 ; i<board.length ; i++){
+            Arrays.fill(board[i],'.');
+        }
+        generate(board,0);
+        return res;
+    }
+    public void generate(char[][] board,int row){
+        if(row==board.length){
+            ArrayList<String> ans = new ArrayList<>();
+            for(int i=0 ; i<board.length ; i++){
+                ans.add(new String(board[i]));
+            }
+            res++;
+            return;
+        }
+        for(int i=0 ; i<board.length ; i++){
+            if(isSafe(board,row,i)){
+                board[row][i] = 'Q';
+                generate(board,row+1);
+                board[row][i] = '.';
+            }
+        }
+    }
+    public boolean isSafe(char[][] board,int row , int col){
+        for(int i=0 ; i<board.length ; i++){
+            if(board[i][col]=='Q'){
+                return false;
+            }
+        }
+        int i=row-1;
+        int j=col-1;
+        while(i>=0 && j>=0){
+            if(board[i][j]=='Q'){
+                return false;
+            }
+            i--;
+            j--;
+        }
+        i=row-1;
+        j=col+1;
+        while(i>=0 && j<board.length){
+            if(board[i][j]=='Q'){
+                return false;
+            }
+            i--;
+            j++;
+        }
+        return true;
+    }
+}
