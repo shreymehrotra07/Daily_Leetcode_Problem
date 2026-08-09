a77d96190cc0bad5a26526f14cd65db537d607b1
Shrey Mehrotra <shreymehrotra011@gmail.com>
2026-08-09 15:47:10 +0530

    Time: 326 ms (44.24%), Space: 43 MB (37.21%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -0,0 +1,45 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+
+    public boolean solve(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == '.') {
+                    for (char c = '1'; c <= '9'; c++) {
+                        if (isValid(board, i, j, c)) {
+                            board[i][j] = c;
+                            if (solve(board) == true) {
+                                return true;
+                            } else {
+                                board[i][j] = '.';
+                            }
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean isValid(char[][] board, int i, int j, int c) {
+        for (int m = 0; m < board.length; m++) {
+            if (board[m][j] == c) {
+                return false;
+            }
+        }
+        for (int m = 0; m < board[0].length; m++) {
+            if (board[i][m] == c) {
+                return false;
+            }
+        }
+        for (int m = 0; m < board.length; m++) {
+            if (board[3 * (i / 3) + m / 3][3 * (j / 3) + m % 3] == c) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
