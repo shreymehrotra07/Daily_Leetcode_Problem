b43ded45a32838b5f0d347eae532fffc0c0e49cf
Shrey Mehrotra <shreymehrotra011@gmail.com>
2026-08-10 22:54:24 +0530

    Time: 14 ms (91.38%), Space: 42.5 MB (78.33%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean winnerSquareGame(int n) {
+
+        boolean[] dp = new boolean[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j * j <= i; j++) {
+
+                int square = j * j;
+                if (dp[i - square] == false) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[n];
+    }
+}
